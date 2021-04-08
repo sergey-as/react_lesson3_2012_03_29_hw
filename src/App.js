f25f5387299b83c543168c500f181fc91f2f1a85
sergey-as <sergey_as@ukr.net>
2021-04-08 20:13:56 +0300

    123

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+const url = 'https://jsonplaceholder.typicode.com/';
+
+const App = () =>{
+    const [list, setList] = useState('posts');
+
+
+const selectPosts = (list) =>{
+    setList({list});
+    console.log(list);
+}
+
+  return(
+      <div className={'App'}>
+
+        {/*<button onClick={selectPosts}>{'posts'}</button>*/}
+        <button onClick={selectPosts('posts')}>{'posts'}</button>
+        {/*<button onClick={''}>{'comments'}</button>*/}
+        {/*<button onClick={''}>{'albums'}</button>*/}
+        {/*<button onClick={''}>{'photos'}</button>*/}
+        {/*<button onClick={''}>{'todos'}</button>*/}
+        {/*<button onClick={''}>{'users'}</button>*/}
+
+
+      </div>
   );
 }
 
 export default App;
+
+
+// дз
+//
+// урл
+// https://jsonplaceholder.typicode.com/
+//
+//     ендпоінти
+//   /posts
+//   /comments
+//   /albums
+//   /photos
+//   /todos
+//   /users
+//
+// потрібно створити логіку, яка задовільнить наступні вимоги
+// в нас має бути 6 кнопок, які дозволяють нам переключатись між 'табами' (posts, comments, albums, photos, todos, users)
+// дефолтно обрана таба- пости
+// по кліку на кнопку ми повинні підтягнути відповідний список і зрендерити його через .map
+// лише 1 список видимий одночасно
+// потрібно створити 6 компонент, які займатимуться рендерінгом списків (отримуватимуть пропсами список)- PostList, CommentsList...
